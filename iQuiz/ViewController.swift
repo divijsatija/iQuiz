de5be385b8559da90ce17dfc8ffa73004edf04f4
divij <divij@uw.edu>
2021-05-10 22:21:53 -0700

    iQuiz part 1 update

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    let myData = myCategorySource()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myTableView.dataSource = myData
+        myTableView.delegate = self
     }
 
-
+    @IBAction func settingsPopUp(_ sender: Any) {
+        let alert = UIAlertController(title: "Settings go here", message: "to be set up", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    func tableView(_ myTableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
+    
+    func tableView(_ myTableView: UITableView, didSelectRowAt: IndexPath) {
+        NSLog("Welcome to iQuiz")
+    }
 }
 
+class myCategorySource: NSObject, UITableViewDataSource {
+    let myCategory = ["Mathematics", "Marvel Super Heroes", "Science"]
+    let myDescription = ["Algebra, Geometry, Calculus", "Iron Man, Thor, Captain America", "Physics, Chemistry, Biology"]
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "BasicStyle", for: indexPath)
+        myCell.textLabel?.text = myCategory[indexPath.row]
+        myCell.detailTextLabel?.text = myDescription[indexPath.row]
+        myCell.imageView?.image = UIImage(named: myCategory[indexPath.row])
+        return myCell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myCategory.count
+    }
+}
